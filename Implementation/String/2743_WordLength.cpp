7efc75c7a564b59e2ec76b33b2026ffb1d8f21ae
Sooyoung Baek <qortndud97@naver.com>
2018-11-28 02:35:08 -0800

    Implementation : String

---
+++ Implementation/String/2743_WordLength.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void){
+    string s;
+    cin >> s;
+    cout << s.length() << endl;
+}
+//Input
+// pulljima
+
+//Output
+// 8
